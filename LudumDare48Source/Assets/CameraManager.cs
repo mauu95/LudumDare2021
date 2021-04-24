4e32c18b8e72cbe84a6de4ce57623db41c33f4e5
mauu95 <mauu95@hotmail.it>
2021-04-24 16:45:28 +0200

    Added Lightning system

---
+++ LudumDare48Source/Assets/CameraManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform toFollow;
+    private float smoothSpeed = 0.125f;
+    public float horizontalOffSet;
+    public float verticalOffSet;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = toFollow.position;
+        if (toFollow.gameObject.CompareTag("Player"))
+        {
+            desiredPosition.x += horizontalOffSet;
+            desiredPosition.y += verticalOffSet;
+        }
+        desiredPosition.z = -1;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+}

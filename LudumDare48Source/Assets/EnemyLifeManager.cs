dd3e78993eb9414671cb87515abdf78057b56872
Taranjit Singh <taran1995s@gmail.com>
2021-04-25 18:26:11 +0200

    Added damage and death sounds for enemies

--- LudumDare48Source/Assets/EnemyLifeManager.cs
+++ LudumDare48Source/Assets/EnemyLifeManager.cs
@@ -6,10 +6,12 @@ public class EnemyLifeManager : MonoBehaviour {
     public float maxLife = 100;
     public GameObject deadFishPrefab;
     protected float currentLife;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start() {
         FillLife();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void FillLife() {
@@ -25,6 +27,8 @@ public class EnemyLifeManager : MonoBehaviour {
             Instantiate(deadFishPrefab, transform.position, transform.rotation, transform.parent);
             this.gameObject.GetComponent<EnemyMovement>().Die();
             FillLife();
+        } else {
+            audioSource.Play();
         }
     }
 

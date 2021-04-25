6cfc69033d1098e89d62ef31f98f73aef4409dda
mauu95 <mauu95@hotmail.it>
2021-04-25 22:04:06 +0200

    Added light radius cap

--- LudumDare48Source/Assets/CardLight.cs
+++ LudumDare48Source/Assets/CardLight.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class CardLight : Card
 {
-    
+    private void Update() {
+        PlayerStats player = PlayerMovement.instance.GetComponent<PlayerStats>();
+        if(player.IsRadiusMaxxedOut())
+            button.gameObject.SetActive(false);
+    }
 }

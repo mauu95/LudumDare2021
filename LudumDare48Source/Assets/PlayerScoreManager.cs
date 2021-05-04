115550328f11ae0e19582041fa8cbff86fd661c9
mauu95 <mauu95@hotmail.it>
2021-05-04 13:38:34 +0200

    Added go down hint

--- LudumDare48Source/Assets/PlayerScoreManager.cs
+++ LudumDare48Source/Assets/PlayerScoreManager.cs
@@ -5,11 +5,16 @@ using TMPro;
 
 public class PlayerScoreManager : MonoBehaviour {
     public TextMeshProUGUI scoreText;
+    public HintUI hint;
     private Transform player;
     int maxScore = 0;
+    private bool warned;
+    
+    private int lastScore;
 
     private void Start() {
         player = Player.instance.transform;
+        InvokeRepeating("DigliDiAndareGiu", 60f, 180f);
     }
 
     void Update() {
@@ -18,9 +23,21 @@ public class PlayerScoreManager : MonoBehaviour {
         float max = Mathf.Max(-player.position.y, maxScore);
         maxScore = (int)max;
         scoreText.text = "Max depth: " + maxScore;
+
+        if(!warned && maxScore > 200){
+            hint.SetText("Warning: SHARKS");
+            warned = true;
+        }
     }
 
     public int GetMaxScore() {
         return maxScore;
+    }
+
+    private void DigliDiAndareGiu(){
+        if(lastScore != maxScore)
+            hint.SetText("GO DOWN");
+        
+        lastScore = maxScore;
     }
 }
